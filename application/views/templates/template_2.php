3411bae3853e4da6a4ba59e08a749383188c7993
abirutama <abirutama@gmail.com>
2020-04-15 10:03:02 +0700

    update template

--- application/views/templates/template_2.php
+++ application/views/templates/template_2.php
@@ -17,17 +17,16 @@
 </head>
 
 <body>
-    <div class="has-background-white box" style="max-width:600px; padding:0px; margin:auto;border-radius:0px 0px 0px 48px !important; padding-bottom:16px; margin-bottom:16px">
+    <div class="" style="max-width:600px; padding:0px; margin:auto;border-radius:0px 0px 0px 48px !important; padding-bottom:8px;">
         <section id="cover" class="">
-            <div class="has-text-centered" style="padding:64px 32px;border-radius:0px 0px 0px 128px !important; background-size:cover;<?= 'background-image:url(\''.base_url().'/assets/img/layout/bg1.jpg\')';  ?>">
+            <div class="has-text-centered" style="padding:64px 32px;border-radius:0px 0px 128px 128px !important; background-size:cover;<?= 'background-image:url(\''.base_url().'/assets/img/layout/bg1.jpg\')';  ?>">
                 <figure class="image is-128x128" style="margin:auto;">
                     <img class="bs is-rounded" src="https://via.placeholder.com/128/?text=<?= scoup(strtoupper(substr($profile['user_name'],0,1))); ?>">
                 </figure>
                 <h2 class="title is-size-5 has-text-white" style="margin-top:8px"><?= $profile['user_name']; ?></h2>
             </div>
-            
         </section>
-        <section style="margin-top:16px">
+        <section class="" style="margin-top:16px;">
             <div class="multiple-items">
                 <?php if($social['social_twitter']){ ?>
                 <a target="_blank" href="https://twitter.com/<?= scoup($social['social_twitter']) ?>"><img class="image bs" width="110px" style="border-radius:32px" src="<?= base_url('assets/img/social-btn/') ?>btn-twitter.png" alt=""></a>
@@ -40,7 +39,42 @@
                 <?php } ?>
             </div>
         </section>
-        <section class="" style="padding:1em; margin-bottom:32px">
+        <section class="has-background-white" style="padding:2em 1em;margin-top:16px;border-radius:8px 8px 8px 48px !important;">
+            <div class="container" style="margin-bottom:24px">
+                <h2 class="title is-size-5">
+                    Pinned Links
+                </h2>
+            </div>
+            <div class="columns is-vcentered multiple-items2">
+                <?php foreach($card as $cardItem){ ?>
+                <div class="column is-full is-vcentered is-centered" style="padding:20px 12px">
+                    <a href="<?= base_url('/@').$profile['user_name'].'/' . $cardItem['card_slug']; ?>" class="media box has-background-white" style="border-radius:64px; padding:8px; margin-bottom:0px; display:flex; align-items:center">
+                        <figure class="media-left">
+                            <?php if($cardItem['card_thumbnail']!== null){ ?>
+                            <p class="image is-64x64">
+                                <img class="is-rounded" src="https://via.placeholder.com/80/f7b780/fffffff?text=<?= strtoupper(substr($cardItem['card_slug'],0,1)); ?>" data-src="<?= $cardItem['card_thumbnail']; ?>">
+                            </p>
+                            <?php } ?>
+                        </figure>
+                        <div class="media-content">
+                            <div class="content">
+                                <p>
+                                    <?php
+                                    if( strlen($cardItem['card_title']) > 50 ){
+                                        echo scoup(substr($cardItem['card_title'],0,50)).'...'; 
+                                    }else{
+                                        echo scoup($cardItem['card_title']);
+                                    }
+                                    ?>
+                                </p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <?php } ?>
+            </div>
+        </section>
+        <section class="has-background-white" style="margin-top:16px;padding:2em 1em; border-radius:8px 8px 8px 48px !important;">
             <div class="container" style="margin-bottom:24px">
                 <h2 class="title is-size-5">
                     My Links
@@ -75,7 +109,11 @@
                 <?php } ?>
             </div>
         </section>
+        
     </div>
+    <figure class="" style="text-align:center; padding:16px">
+        <img width="60px" src="<?= base_url('assets/img/layout/') ?>footer.png" alt="">
+    </figure>
 </body>
 <script>
 $(document).ready(function() {
@@ -102,6 +140,34 @@ $(document).ready(function() {
                     slidesToScroll: 1,
                     centerMode: true,
                     centerPadding: '80px'
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+    $('.multiple-items2').slick({
+        infinite: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        responsive: [{
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    infinite: true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
                 }
             }
             // You can unslick at a given breakpoint now by adding:
