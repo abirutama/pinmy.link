5097c8e036865c63ce989eacde8418c77764262a
abirutama <abirutama@gmail.com>
2020-08-09 01:57:09 +0700

    update

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -55,18 +55,24 @@ class Auth extends CI_Controller {
 		$this->load->view('auth/google_v1');
 	}
 	public function sign_google(){
+		
 		$username_auto = $this->input->post('email', true);
-		$username_auto = str_replace("@gmail.com","",$username_auto);
-		$data_user = [
-			'user_name' => $username_auto,
-			'user_email' => htmlspecialchars($this->input->post('email', true)),
-			'user_pass' => password_hash($this->input->post('email'), PASSWORD_DEFAULT),
-			'role_id' => 2,
-			'is_active' => 1,
-			'date_created' => time()
-		];
-		$this->db->insert('user', $data_user);
-		$new_user_id = $this->db->insert_id();
+		$user = $this->db->get_where('user', ['user_email' => $username_auto])->row_array();
+		if($user){
+			redirect('user');
+		}else{
+			$username_auto = str_replace("@gmail.com","",$username_auto);
+			$data_user = [
+				'user_name' => $username_auto,
+				'user_email' => htmlspecialchars($this->input->post('email', true)),
+				'user_pass' => password_hash($this->input->post('email'), PASSWORD_DEFAULT),
+				'role_id' => 2,
+				'is_active' => 1,
+				'date_created' => time()
+			];
+			$this->db->insert('user', $data_user);
+			$new_user_id = $this->db->insert_id();
+		}
 	}
 
 	public function register()
--- application/views/auth/google_v1.php
+++ application/views/auth/google_v1.php
@@ -15,10 +15,6 @@
 <script>
     function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
     $.post( "<?= base_url('auth/sign_google') ?>",{email: profile.getEmail()}).done(function( data ) {
         alert( "Data Loaded: " + data );
     });
