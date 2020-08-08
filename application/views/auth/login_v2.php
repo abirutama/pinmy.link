82a7985427285d8b4e570e775a62cbae4f7d1537
abirutama <abirutama@gmail.com>
2020-08-08 23:59:45 +0700

    Update login_v2.php

--- application/views/auth/login_v2.php
+++ application/views/auth/login_v2.php
@@ -26,6 +26,7 @@
                 <div class="control">
                     <button class="button is-link is-fullwidth">Sign In</button>
                     <div class="g-signin2" data-onsuccess="onSignIn">Sign With Google</div>
+                    <div class="g-signout2" data-onsuccess="onSignOut">Signout</div>
                 </div>
 
                 <hr>
