388ede543ab72e597342f2c291db35302883d3b3
abirutama <abirutama@gmail.com>
2020-08-11 03:29:08 +0700

    xx

--- application/views/user/userpanel_header_v2.php
+++ application/views/user/userpanel_header_v2.php
@@ -35,19 +35,7 @@
     }
     </style>
 </head>
-<script>
-gapi.load('auth2', function(){
-    gapi.auth.init();
-});
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        console.log('User signed out.');
-        var urlr = "<?= base_url('auth/logout') ?>";
-        $(location).attr('href', urlr);
-    });
-}
-</script>
+
 
 <body>
     <nav class="navbar" role="navigation" aria-label="main navigation" style="border-top:6px #3298dc solid">
