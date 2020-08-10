fbf2b7306db903a967ae3bc2747655f931f1b2bf
abirutama <abirutama@gmail.com>
2020-08-11 03:33:23 +0700

    Update userpanel_footer_v2.php

--- application/views/user/userpanel_footer_v2.php
+++ application/views/user/userpanel_footer_v2.php
@@ -13,7 +13,7 @@
 <script>
 $( document ).ready(function() {
   gapi.load('auth2', function(){
-      gapi.auth.init();
+      gapi.auth2.init();
   });
   function signOut() {
       var auth2 = gapi.auth2.getAuthInstance();
