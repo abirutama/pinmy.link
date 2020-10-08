8c46f59df3170894002973dc330dc6660ec32951
abirutama <abirutama@gmail.com>
2020-10-08 13:31:54 +0700

    update favicon

--- application/views/webpage/index.php
+++ application/views/webpage/index.php
@@ -2,18 +2,33 @@
 <html>
 
 <head>
-    <title>The Best Way to Create Sitemap Across Your Content And Social Media | Pin My Link | Pinmy.link</title>
+    <title>Satu halaman untuk semua kontenmu! | Pinmy.link</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description"
-        content="How to make an easy sitemap across your content and social media. Easy to register, find out now!">
+        content="Mudahkan audiens untuk mengakses semua kontenmu dalam satu halaman! Yuk gunakan Pinmy.link sekarang juga!">
     <meta name="robots" content="follow, index">
-
+    <link rel="apple-touch-icon" sizes="57x57" href="<?= base_url('assets'); ?>/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="<?= base_url('assets'); ?>/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?= base_url('assets'); ?>/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?= base_url('assets'); ?>/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?= base_url('assets'); ?>/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?= base_url('assets'); ?>/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="<?= base_url('assets'); ?>/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="<?= base_url('assets'); ?>/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= base_url('assets'); ?>/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="<?= base_url('assets'); ?>/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= base_url('assets'); ?>/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="<?= base_url('assets'); ?>/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url('assets'); ?>/favicon/favicon-16x16.png">
+    <link rel="manifest" href="<?= base_url('assets'); ?>/favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="<?= base_url('assets'); ?>/favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
     <link rel=”canonical” href=”https://pinmy.link/” />
     <link rel=”canonical” href=”https://www.pinmy.link/” />
     <link rel=”canonical” href=”http://pinmy.link/” />
     <link rel=”canonical” href=”http://www.pinmy.link/” />
-
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
     <!--<script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>-->
@@ -119,13 +134,16 @@
             </div>
             <div class="column" style="background-image:url('<?= base_url('assets/img'); ?>/frontpage/new/people_holdphone.jpg');background-size:contain; background-repeat:no-repeat; background-position:right">
                 <h4 class="title is-4">2. Masukan Kontenmu</h4>
-                <p style="margin-right:48px; background:#ffffff70; border-radius:8px">Masukan konten yang akan kamu sajikan kepada audiens ke dalam halaman kamu. Perbarui konten secara berkala dengan langkah mudah.</p><br>
+                <p style="margin-right:48px; background:#ffffff70; border-radius:8px">Masukan konten yang akan kamu sajikan kepada audiens ke dalam halaman kamu. Perbarui konten secara berkala dengan mudah.</p><br>
             </div>
             <div class="column" style="background-image:url('<?= base_url('assets/img'); ?>/frontpage/new/people_ok.jpg');background-size:contain; background-repeat:no-repeat; background-position:right">
                 <h4 class="title is-4">3. Pasang di Bio-mu</h4>
                 <p style="margin-right:48px; background:#ffffff70; border-radius:8px">Sematkan link unik <strong>pinmy.link/milikmu</strong> di semua bio kanalmu, dan audiens akan mudah akses konten kamu.</p><br>
             </div>
-
+        </div>
+    </section>
+    <section>
+        <div>
         </div>
     </section>
     <footer class="footer">
