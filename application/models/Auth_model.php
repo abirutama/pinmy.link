15a72e6043a83bb190259fdf7d711ea42ed4f197
abirutama <abirutama@gmail.com>
2020-12-11 03:17:35 +0700

    asd

--- application/models/Auth_model.php
+++ application/models/Auth_model.php
@@ -5,25 +5,26 @@
         {
                 $config = [
                         'protocol' => 'smtp',
-                        'smtp_host' => 'ssl://mail.pinmy.link',
+                        'smtp_host' => 'mail.pinmy.link',
                         'smtp_user' => 'no-reply@pinmy.link',
                         'smtp_timeout' => 5,
                         'smtp_pass' => '~~pinmylink~~',
                         'smtp_port' => 465,
-                        'mailtype' => 'html',
+                        'smtp_crypto' => 'ssl',
+                        'mailtype' => 'text',
                         'charset' => 'utf-8',
                         'newline' => "\r\n"
                 ];
                 $this->load->library('email', $config);
 
-                $this->email->from('no-reply@pinmy.link', 'PINMY.LINK');
+                $this->email->from('no-reply@pinmy.link');
                 $this->email->to('hello@abirutama.com');
                 $this->email->subject('User Activation');
                 $this->email->message('Hello, please kindly verify your account by clicking url below:');
 
                 if ($this->email->send())
                 {
-                        echo $this->email->print_debugger();
+                        //echo $this->email->print_debugger();
                 }else{
                         echo $this->email->print_debugger();
                 }
