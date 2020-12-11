2828f28328e26c053c4d9eef09ea6165d7870112
abirutama <abirutama@gmail.com>
2020-12-11 10:42:04 +0700

    Update Auth_model.php

--- application/models/Auth_model.php
+++ application/models/Auth_model.php
@@ -6,19 +6,19 @@
                 $this->load->library('email');
 
                 $config['protocol']    = 'smtp';
-$config['smtp_host']    = 'mail.pinmy.link';
-$config['smtp_port']    = '465';
-$config['smtp_timeout'] = '7';
-$config['smtp_user']    = 'no-reply@pinmy.link';
-$config['smtp_pass']    = '~~pinmylink~~';
-$config['charset']    = 'utf-8';
-$config['newline']    = "\r\n";
-$config['mailtype'] = 'text'; // or html
-$config['validation'] = TRUE; // bool whether to validate email or not      
+                $config['smtp_host']    = 'ssl://mail.rupakara.com';
+                $config['smtp_port']    = '465';
+                $config['smtp_timeout'] = '4';
+                $config['smtp_user']    = 'cornel70@rupakara.com';
+                $config['smtp_pass']    = 'rctiplus123';
+                $config['charset']    = 'utf-8';
+                $config['newline']    = "\r\n";
+                $config['mailtype'] = 'text'; // or html
+                $config['validation'] = TRUE; // bool whether to validate email or not      
 
 $this->email->initialize($config);
 
-                $this->email->from('no-reply@pinmy.link');
+                $this->email->from('cornel70@rupakara.com');
                 $this->email->to('abirutama@gmail.com');
                 $this->email->subject('User Activation');
                 $this->email->message('Hello, please kindly verify your account by clicking url below:');
