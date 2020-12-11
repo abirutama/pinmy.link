e69385be77648754aeb37e7238f55c8132027486
abirutama <abirutama@gmail.com>
2020-12-11 10:57:17 +0700

    Update Auth_model.php

--- application/models/Auth_model.php
+++ application/models/Auth_model.php
@@ -8,9 +8,9 @@
                 $config['protocol']    = 'smtp';
                 $config['smtp_host']    = 'ssl://mail.rupakara.com';
                 $config['smtp_port']    = '465';
-                $config['smtp_timeout'] = '4';
-                $config['smtp_user']    = 'cornel70@rupakara.com';
-                $config['smtp_pass']    = 'rctiplus123';
+                $config['smtp_timeout'] = '3';
+                $config['smtp_user']    = 'no-reply@pinmy.link';
+                $config['smtp_pass']    = '~~webmail~~';
                 $config['charset']    = 'utf-8';
                 $config['newline']    = "\r\n";
                 $config['mailtype'] = 'text'; // or html
@@ -18,7 +18,7 @@
 
 $this->email->initialize($config);
 
-                $this->email->from('cornel70@rupakara.com');
+                $this->email->from('no-reply@pinmy.link');
                 $this->email->to('abirutama@gmail.com');
                 $this->email->subject('User Activation');
                 $this->email->message('Hello, please kindly verify your account by clicking url below:');
